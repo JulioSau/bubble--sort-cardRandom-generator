39713b2d5c9238c79e40a189b01b6515783e7c85
JulioSau <tura_96@hotmail.com>
2020-08-02 18:04:18 +0000

    numero de cartas random

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,9 @@ import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
-window.onload = function() {
+window.onload = function() {};
+
+window.drawCards = function drawCards() {
   let symbols = ["&clubs;", "&spades;", "&hearts;", "&diams;"];
   let cardLetter = [
     "A",
@@ -22,23 +24,70 @@ window.onload = function() {
     "Q",
     "K"
   ];
+  let spaceRandomCard = document.querySelector("#spaceRandomCard");
+  spaceRandomCard.innerHTML = "";
+  let newInput = document.querySelector("input").value;
 
-  let symbolRandom = Math.floor(Math.random() * symbols.length);
-  let letterRandom = Math.floor(Math.random() * cardLetter.length);
+  for (let i = 0; i < newInput; i++) {
+    let symbolRandom = Math.floor(Math.random() * symbols.length);
+    let letterRandom = Math.floor(Math.random() * cardLetter.length);
+    let spaceRandomCard = document.querySelector("#spaceRandomCard");
+    if (i == newInput - 1) {
+      spaceRandomCard.insertAdjacentHTML(
+        "afterbegin",
+        `<div class="carta ml-5 bg-white col-1 mt-5">
+        <div class="row pl-1">
+          <p class="h1" id="icon">
+            &clubs;
+          </p>
+        </div>
+        <div class="row justify-content-center">
+          <p class="letter" id="letter">
+            J
+          </p>
+        </div>
+        <div class="row justify-content-end pr-1">
+          <p class="h1" id="icon">
+            &clubs;
+          </p>
+        </div>
+      </div>`
+      );
+    } else {
+      spaceRandomCard.insertAdjacentHTML(
+        "afterbegin",
+        `<div class="carta ml-3 bg-white col-1 mt-5">
+        <div class="row pl-1">
+          <p class="h1" id="icon">
+            &clubs;
+          </p>
+        </div>
+        <div class="row justify-content-center">
+          <p class="letter" id="letter">
+            J
+          </p>
+        </div>
+        <div class="row justify-content-end pr-1">
+          <p class="h1" id="icon">
+            &clubs;
+          </p>
+        </div>
+      </div>`
+      );
+    }
 
-  let newLetter = document.querySelector("#letter");
-  newLetter.innerHTML = cardLetter[letterRandom];
-
-  let newSymbol = document.querySelectorAll("#icon");
-  newSymbol[0].innerHTML = symbols[symbolRandom];
-  newSymbol[1].innerHTML = symbols[symbolRandom];
-
-  if (
-    newSymbol[0].innerHTML == "\u2665" ||
-    newSymbol[0].innerHTML == "\u2666"
-  ) {
-    newSymbol[0].style.color = "red";
-    newSymbol[1].style.color = "red";
-    newLetter.style.color = "red";
+    let newLetter = document.querySelector("#letter");
+    newLetter.innerHTML = cardLetter[letterRandom];
+    let newSymbol = document.querySelectorAll("#icon");
+    newSymbol[0].innerHTML = symbols[symbolRandom];
+    newSymbol[1].innerHTML = symbols[symbolRandom];
+    if (
+      newSymbol[0].innerHTML == "\u2665" ||
+      newSymbol[0].innerHTML == "\u2666"
+    ) {
+      newSymbol[0].style.color = "red";
+      newSymbol[1].style.color = "red";
+      newLetter.style.color = "red";
+    }
   }
 };
